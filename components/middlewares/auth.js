67434819209b8c25339d613d8ef76d0c460a1908
OJailson17 <JaylsonO17@gmail.com>
2021-05-25 04:59:16 -0300

    adding jwt validation

---
+++ components/middlewares/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import config from '../../config/index.js'
+
+export default (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+        res.status(401).json({error: "No token privided"})
+    } else {
+        const token = authHeader.replace('Bearer ', '')
+
+        jwt.verify(token, config.SECRET_HASH_TOKEN, (err,decoded) => {
+            if(err) {
+                res.status(401).json({error: "Invalid token"})
+            } else {
+                req.userId = decoded.id
+                return next()
+            }
+        })
+    }
+}
