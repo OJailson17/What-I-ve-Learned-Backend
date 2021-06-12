369bee25fade731a1f33aa3756bbc8788fb2b411
OJailson17 <JaylsonO17@gmail.com>
2021-06-12 00:17:13 -0300

    adding override dependency

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import methodOverride from 'method-override'
+import cors from 'cors'
 
 import config from "./config/index.js";
 import db from "./database/db.js";
@@ -10,6 +12,8 @@ const { PORT, MONGODB_URI } = config;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+app.use(cors())
 
 routes(app);
 db(MONGODB_URI);
