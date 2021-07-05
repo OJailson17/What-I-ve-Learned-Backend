9aa22ba0e9050b3a1ff97fa1511c30e2b9426b22
OJailson17 <JaylsonO17@gmail.com>
2021-07-05 01:10:57 -0300

    correcting cors bug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const { MONGODB_URI } = config;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
-app.use(cors({origin: "https://what-i-ve-learned-frontend.herokuapp.com/"}))
+app.use(cors())
 
 routes(app);
 db(MONGODB_URI);
